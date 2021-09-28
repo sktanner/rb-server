bb96062c83cd3515675e650deedba497836aa79c
Sarah Tanner <sktanner92@gmail.com>
2021-09-28 12:20:16 -0400

    changed desc type to text

--- models/game.js
+++ models/game.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
         },
         description: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false,
         },
         thumb_url: {
